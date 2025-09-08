70910196a98a7d9ec52da8e0a2cb1c3654b9ec94
rayssakesia <rayssakesialo@gmail.com>
2025-09-08 12:45:07 -0300

    "..."

--- shared/src/main/java/com/rayssa/shared/MyClass.kt
+++ shared/src/main/java/com/rayssa/shared/MyClass.kt
@@ -1,33 +1,6 @@
 package com.rayssa.shared
 
-//import io.ktor.client.HttpClient
-//import io.ktor.client.call.body
-//import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-//import io.ktor.client.request.get
-//import io.ktor.client.statement.bodyAsText
-//import io.ktor.serialization.kotlinx.json.json
-//import kotlinx.serialization.Serializable
-//
-//@Serializable
-//data class User(val name: String, val age: Int)
-//
-//class ApiService {
-//    private val client = HttpClient {
-//        install(ContentNegotiation) {
-//            json()
-//        }
-//    }
-//
-//    suspend fun getAnswer(): String {
-//        return client.get("http://10.0.2.2:8080/users").body()
-//    }
-//}
-
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import retrofit2.http.GET
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
 object ApiClient {
